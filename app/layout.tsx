6e1e11c241f46186ea17e00e797b3a67a984f1ca
Joyal George K J <joyalgeorgekj@gmail.com>
2026-04-18 16:44:42 +0530

    Header and Footer moved to layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,38 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "./Header";
+import Footer from "./Footer";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Joyal George K J",
-  description: "React/Next js Developer Portfolio - 2026, Joyal George K J",
+    title: "Joyal George K J",
+    description: "React/Next js Developer Portfolio - 2026, Joyal George K J",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <h1>Hey</h1>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
