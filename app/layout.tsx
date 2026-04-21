18b2b2060935915d19977d9ad4a262b2d8b8d8c2
Joyal George K J <joyalgeorgekj@gmail.com>
2026-04-22 03:09:05 +0530

    Header, Footer and Background plus other repeating contents moved from its own page to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,21 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased w-[99dvw]`}
             >
                 <Header />
-                {children}
+                <main className="relative w-full bg-black text-white">
+                    {/* Background Effects */}
+                    <div className="pointer-events-none absolute inset-0">
+                        <div className="absolute left-0 top-20 h-72 w-72 rounded-full bg-cyan-500/10 blur-3xl" />
+                        <div className="absolute right-5 top-40 h-80 w-80 rounded-full bg-violet-500/10 blur-3xl" />
+                        <div className="absolute bottom-10 left-1/3 h-64 w-64 rounded-full bg-blue-500/10 blur-3xl" />
+
+                        {/* Grid */}
+                        <div className="absolute inset-0 bg-[linear-gradient(to_right,rgba(255,255,255,.03)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,.03)_1px,transparent_1px)] bg-size-[40px_40px]" />
+                    </div>
+                    {children}
+                </main>
                 <Footer />
             </body>
         </html>
