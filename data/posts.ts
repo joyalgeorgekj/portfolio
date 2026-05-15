521ee0c97d5707cac4524335b863a3c6774038f4
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-16 02:52:47 +0530

    global post and skill data created

---
+++ data/posts.ts
@@ -0,0 +1,6 @@
+export const posts = [
+    "Building scalable frontend systems",
+    "How to optimize React performance",
+    "Shipping products as a solo developer",
+    "Lessons from open source contributions",
+];
---
+++ data/skills.ts
@@ -0,0 +1,81 @@
+export const skills = {
+    frontend: [
+        {
+            title: "React",
+            icon: "react/react-original.svg"
+        },
+        {
+            title: "Next.js",
+            icon: "nextjs/nextjs-original.svg"
+        },
+        {
+            title: "TypeScript",
+            icon: "typescript/typescript-original.svg"
+        },
+        {
+            title: "Redux",
+            icon: "redux/redux-original.svg"
+        },
+        {
+            title: "JavaScript",
+            icon: "javascript/javascript-original.svg"
+        }
+    ],
+    styling: [
+        {
+            title: "Tailwind CSS",
+            icon: "tailwindcss/tailwindcss-original.svg"
+        },
+        {
+            title: "shadcn/ui",
+            icon: ""
+        },
+        {
+            title: "Framer Motion",
+            icon: "framermotion/framermotion-original.svg"
+        },
+        {
+            title: "GSAP",
+            icon: ""
+        }
+    ],
+    quality: [
+        {
+            title: "Performance",
+            icon: ""
+        },
+        {
+            title: "Accessibility",
+            icon: ""
+        },
+        {
+            title: "SEO",
+            icon: ""
+        },
+        {
+            title: "Responsive UI",
+            icon: ""
+        }
+    ],
+    tooling: [
+        {
+            title: "Git",
+            icon: "git/git-original.svg"
+        },
+        {
+            title: "Docker",
+            icon: "docker/docker-original.svg"
+        },
+        {
+            title: "Appwrite",
+            icon: "appwrite/appwrite-original.svg"
+        },
+        {
+            title: "Linux",
+            icon: "linux/linux-original.svg"
+        },
+        {
+            title: "Vercel",
+            icon: "vercel/vercel-original.svg"
+        }]
+    };
