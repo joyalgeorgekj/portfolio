7228db34e7fbdbb8b9b5f95c0764b6f6902eccd8
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-16 02:51:15 +0530

    global achievements data and type created

---
+++ data/achievements.ts
@@ -0,0 +1,27 @@
+import { Achievement } from "@/types/achievements";
+
+export const ACHIEVEMENTS: Achievement[] = [
+    {
+        id: "hacktoberfest-2025",
+        title: "Hacktoberfest Super Contributor",
+        description:
+            "Ranked among the top 10,000 contributors globally with 9 badges earned.",
+        organization: "Hacktoberfest",
+        type: "Open Source",
+    },
+
+    {
+        id: "npm-users",
+        title: "1000+ Weekly npm Users",
+        description:
+            "Published reusable frontend tooling actively used by developers.",
+        type: "Product",
+    },
+    {
+        id: "tecza-web-design",
+        title: "Winner — Web Designing Competition",
+        organization: "Tecza Inter-University Fest",
+        type: "Competition",
+        description: ""
+    },
+];
---
+++ types/achievements.ts
@@ -0,0 +1,12 @@
+export interface Achievement {
+  id: string;
+  title: string;
+  description: string;
+  organization?: string;
+  type:
+    | "Award"
+    | "Open Source"
+    | "Competition"
+    | "Recognition"
+    | "Product";
+}
