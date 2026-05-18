5a7d09a8426e8b36ca6ce05d397add9b09ea78c7
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-18 18:50:01 +0530

    Generated SEO Metadata for Dynamic blog items

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,11 +2,26 @@ import BlogCard from "@/components/cards/BlogCard";
 import { BLOGS } from "@/content/blog/blog";
 import { extractor } from "@/lib/extract";
 import { Blog } from "@/types/blog.type";
+import { Metadata } from "next";
 
 interface Props {
     params: Promise<{
         slug: string;
     }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  // Option A: Clean up the slug text to look like a title (e.g., "my-first-post" -> "My First Post")
+  
+    const blog: Blog = BLOGS.filter((blog) => blog.id === slug)[0] ?? false;
+
+  return {
+    title: blog.title,
+    description: blog.prevDescription,
+    keywords: blog.tags,
+  };
 }
 
 export default async function BlogSlugPage({ params }: Props) {
