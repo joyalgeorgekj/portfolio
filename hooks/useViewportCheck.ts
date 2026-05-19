d89ea5cb2a7c080c1af4349ea3a3bbc658d8d2c2
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-19 20:01:26 +0530

    hook to check if scolled into viewport added

---
+++ hooks/useViewportCheck.ts
@@ -0,0 +1,32 @@
+import { RefObject, useEffect, useState } from 'react'
+
+function useViewportCheck(cardRef: RefObject<HTMLElement | null>) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        setIsVisible(true);
+                    }
+                });
+            },
+            { threshold: 0.2 }
+        );
+
+        if (cardRef.current) {
+            observer.observe(cardRef.current);
+        }
+
+        return () => {
+            if (cardRef.current) {
+                observer.unobserve(cardRef.current);
+            }
+        };
+    }, []);
+
+  return isVisible
+}
+
+export default useViewportCheck
