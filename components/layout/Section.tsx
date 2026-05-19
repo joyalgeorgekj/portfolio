cbd97e362e46ce6603d1aa9c289003560c14dbd1
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-19 20:00:29 +0530

    added animation to sections component

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import useViewportCheck from "@/hooks/useViewportCheck";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 
 export default function Section({
     children,
@@ -8,28 +11,42 @@ export default function Section({
     linkNeeded = true,
     id,
     container = "mt-6 space-y-3",
+    sectionClass,
 }: Readonly<{
     id: string;
     link?: string;
-    heading: string;
+    heading?: string;
     children: ReactNode;
     container?: string;
     linkNeeded?: boolean;
+    sectionClass?: string;
 }>) {
-    return (
-        <section id={id} className="rounded-3xl bg-background p-8 scroll-mt-28">
-            <div className="flex items-center justify-between">
-                <h2 className="text-sm uppercase tracking-[0.2em] text-typography/75">
-                    {heading}
-                </h2>
-                {linkNeeded && link && (
-                    <Link href={link} className="text-sm text-primary">
-                        View More
-                    </Link>
-                )}
-            </div>
+    const sectionRef = useRef<HTMLElement | null>(null);
+    const isVisible = useViewportCheck(sectionRef);
 
-            <div className={container}>{children}</div>
+    return (
+        <section
+            id={id}
+            className={`${sectionClass ? sectionClass : "rounded-3xl bg-background p-8 scroll-mt-28"} opacity-0 fadeIn ${isVisible ? "in-view" : ""}`}
+            ref={sectionRef}>
+            {heading && (
+                <div className="flex items-center justify-between">
+                    <h2 className="text-sm uppercase tracking-[0.2em] text-typography/75">
+                        {heading}
+                    </h2>
+                    {linkNeeded && link && (
+                        <Link href={link} className="text-sm text-primary">
+                            View More
+                        </Link>
+                    )}
+                </div>
+            )}
+
+            {sectionClass ? (
+                <>{children}</>
+            ) : (
+                <div className={container}>{children}</div>
+            )}
         </section>
     );
 }
