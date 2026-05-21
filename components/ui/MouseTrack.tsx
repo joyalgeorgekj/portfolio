7f6fbe1d0c813a33ba8f12f0acaec77878633585
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-21 23:12:43 +0530

    Mouse tracker improved with desktop only

--- components/ui/MouseTrack.tsx
+++ components/ui/MouseTrack.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function MouseTrack() {
     const cursorRef = useRef<HTMLDivElement>(null);
+    const [desktop, setDesktop] = useState<number | null>(null);
 
     useEffect(() => {
         const moveCursor = (e: MouseEvent) => {
@@ -13,15 +14,20 @@ export default function MouseTrack() {
             }
         };
 
-        window.addEventListener("mousemove", moveCursor);
-        return () => window.removeEventListener("mousemove", moveCursor);
-    }, []);
+        setDesktop(window.innerWidth);
+
+        if (desktop && desktop > 800) {
+            window.addEventListener("mousemove", moveCursor);
+
+            return () => window.removeEventListener("mousemove", moveCursor);
+        }
+    }, [desktop]);
 
     return (
         <div
             ref={cursorRef}
-            className="pointer-events-none fixed left-0 top-0 z-9999 h-10 w-10 
-                 rounded-full bg-background mix-blend-difference"
+            className="hidden md:flex pointer-events-none fixed left-0 top-0 z-9999 h-10 w-10 
+                 rounded-full bg-typography mix-blend-difference"
             style={{
                 // Use transform for hardware acceleration (smoother than top/left)
                 // transform: `translate3d(calc(var(--x) - 20px), calc(var(--y) - 20px), 0)`,
