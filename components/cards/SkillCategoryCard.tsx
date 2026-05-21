b8b63f355db635735b553e9917c021218003c035
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-21 23:07:29 +0530

    Skill category card created

---
+++ components/cards/SkillCategoryCard.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import useViewportCheck from "@/hooks/useViewportCheck";
+import { Skill} from "@/types/skill.type";
+import { useRef } from "react";
+import SkillCard from "./SkillCard";
+
+interface Props {
+    category: string;
+    skills: Skill[]
+}
+
+export default function SkillCategoryCard({ category, skills }: Props) {
+    const cardRef = useRef<HTMLDivElement | null>(null);
+    const isVisible = useViewportCheck(cardRef);
+
+    return (
+        <div
+            ref={cardRef}
+            aria-label={'Skill Category "' + category + '"'}
+            className={`group card rounded-2xl p-5 opacity-0 fade-in ${isVisible ? "in-view" : ""}`}>
+            <h3 className="capitalize text-sm font-semibold text-typography/75">
+                {category}
+            </h3>
+
+            <div className="mt-4 flex flex-wrap gap-2">
+                {skills.map((item) => (
+                    <SkillCard skill={item} key={item.title} />
+                ))}
+            </div>
+        </div>
+    );
+}
