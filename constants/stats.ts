1d289a03f4a4e3394fa31035a25b20a60f00cb20
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-21 23:13:00 +0530

    Reusing stats

---
+++ constants/stats.ts
@@ -0,0 +1,8 @@
+export const stats = [
+        new Date().getFullYear() -
+            new Date("2023").getFullYear() +
+            "+ Years Experience",
+        "1500+ Weekly npm Users",
+        "Open Source Contributor",
+        "Production React Apps",
+    ];
