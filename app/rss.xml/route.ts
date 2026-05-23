0c22a55082dc33e9541d612d995a189b9fbfa46f
Joyal George K J <joyalgeorgekj@gmail.com>
2026-05-24 03:23:39 +0530

    RSS updated

--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -6,39 +6,23 @@ export async function GET() {
         (post) => `
         <item>
             <title><![CDATA[${post.title}]]></title>
-
             <description><![CDATA[${post.prevDescription}]]></description>
-
-            <link>
-                ${BASE_URL}/blog/${post.id}
-            </link>
-
-            <guid>
-                ${BASE_URL}/blog/${post.id}
-            </guid>
-
-            <pubDate>
-                ${new Date(post.publishedAt).toUTCString()}
-            </pubDate>
+            <link>${BASE_URL}/blog/${post.id}</link>
+            <guid>${BASE_URL}/blog/${post.id}</guid>
+            <pubDate>${new Date(post.publishedAt).toUTCString()}</pubDate>
         </item>
     `
     ).join("");
 
-    const xml = `
-        <?xml version="1.0" encoding="UTF-8" ?>
+    const xml = `<?xml version="1.0" encoding="UTF-8" ?>
         
-        <rss version="2.0">
+        <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
             <channel>
                 <title>${NAME}</title>
-
-                <description>
-                    ${DESCRIPTION}
-                </description>
-
-                <link>
-                    ${BASE_URL}
-                </link>
-
+                <description>${DESCRIPTION}</description>
+                <link>${BASE_URL}</link>
+                <language>en-us</language>
+                <atom:link href="${BASE_URL}/rss.xml" rel="self" type="application/rss+xml" />
                 ${posts}
             </channel>
         </rss>
