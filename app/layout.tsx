debf0b73aa6b4648c4c7f30f0dadf077ee110f32
Joyal George K J <joyalgeorgekj@gmail.com>
2026-06-01 19:34:44 +0530

    Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -112,6 +112,15 @@ export default function RootLayout({
 
     return (
         <html lang="en">
+            <head>
+                <script type="text/javascript">
+                    {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x097n0tbjg");`}
+                </script>
+            </head>
             <body className={`antialiased`}>
                 <Header />
                 <main className="relative w-full text-typography">
